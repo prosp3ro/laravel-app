319c4adae630fdf3a0af37be391a65346de4c91f
prosp3ro <g0lden_vo1d@protonmail.com>
2023-12-13 11:50:57 +0100

    vite: tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,16 @@
-<x-star-rating />
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>App Name</title>
+
+</head>
+
+<body>
+    <x-star-rating />
+</body>
+
+</html>
